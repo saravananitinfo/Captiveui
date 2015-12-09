549d53c385f7f92ebbea84a257fcb3f9a2ebc9ff
akshay <akshay@wavespot.net>
2015-12-09 20:17:17 +0530

    Editor button color and radius changes

--- app/view/editor/ButtonWidget.js
+++ app/view/editor/ButtonWidget.js
@@ -7,11 +7,16 @@ Ext.define("CaptivePortal.view.editor.ButtonWidget",{
 	width: '100%',
     height: '100%',
     cls: 'button_widget',
-    button_json: '{"text":"Default","padding_val":5,"font_size":13}',
+    // layout: 'hbox',
+    button_json: '{"text":"Default","padding_val":5,"font_size":13,txt_color:"",bg_color:"",border_radius: 0}',
     // style: 'border: solid 1px gray',
+    // ddGroup:"akshay",
     header: {
-        hidden: true,
+        // hidden: true,
         titlePosition: 0,
+        style:{
+            background: 'transparent'
+        },
         title: 'Button',
         items:[{
             xtype:'button',
@@ -26,6 +31,7 @@ Ext.define("CaptivePortal.view.editor.ButtonWidget",{
                     xtype: 'editor_button_setting',
                     cls: this.up(".button_widget").id,
                     button_id: this.up(".button_widget").id,
+                    btn_json: this.up(".button_widget").button_json,
                     header: {
                         titlePosition: 0,
                         title: 'Button Settings'
@@ -69,9 +75,14 @@ Ext.define("CaptivePortal.view.editor.ButtonWidget",{
                     }
                 }
 
-                var theme = this.up().up().up();
-                var indx = theme.items.indexOf(this.up().up());
-                theme.remove(this.up().up());
+                // var theme = this.up().up().up();
+                // var indx = theme.items.indexOf(this.up().up());
+                // theme.remove(this.up().up());
+
+                var theme = this.up('.button_widget').up();
+                var indx = theme.items.indexOf(this.up('.button_widget'));
+                theme.remove(this.up('.button_widget'));
+
                 theme.insert(indx,{
                     xtype: "dropPanel",
                     cls: "dropPanel",
@@ -81,26 +92,111 @@ Ext.define("CaptivePortal.view.editor.ButtonWidget",{
             }
         }]
     },
+    // tools: [{
+    //     type:'gear',
+    //     // tooltip: 'Refresh',
+    //     handler: function(event, toolEl, panel){
+    //         console.log("akshay..............");
+    //         console.log(this.up(".button_widget").id);
+    //         var editor_settings = Ext.ComponentQuery.query('#editor_settings')[0];
+    //         var panel = editor_settings.down('#editor_setting_panel');
+    //         panel.removeAll();
+    //         panel.add({
+    //             xtype: 'editor_button_setting',
+    //             cls: this.up(".button_widget").id,
+    //             button_id: this.up(".button_widget").id,
+    //             btn_json: this.up(".button_widget").button_json,
+    //             header: {
+    //                 titlePosition: 0,
+    //                 title: 'Button Settings'
+    //             },
+    //             listeners: {
+    //                 'close': function() {
+    //                     var editor_settings = Ext.ComponentQuery.query('#editor_settings')[0]
+    //                     editor_settings.setActiveItem(0)
+    //                 }
+    //             }
+    //         });
+
+    //         editor_settings.setActiveItem(1)
+    //     }
+    // },{
+    //      type:'close',
+    //      handler: function(){
+    //         // var b_setting = Ext.query('.'+this.up(".button_widget").id)[0]
+    //         var setting = Ext.ComponentQuery.query('#editor_settings')[0];
+    //         console.log(setting.getLayout().activeItem);
+
+    //         if(setting.getLayout().getActiveItem().itemId === 'editor_setting_panel'){
+    //             if(setting.getLayout().getActiveItem().down('editor_button_setting').button_id == this.up('.button_widget').id){
+    //                 setting.setActiveItem(0);
+    //             }
+    //         }
+
+    //         // var theme = this.up().up().up();
+    //         // var indx = theme.items.indexOf(this.up().up());
+    //         // theme.remove(this.up().up());
+
+    //         var theme = this.up('.button_widget').up();
+    //         var indx = theme.items.indexOf(this.up('.button_widget'));
+    //         theme.remove(this.up('.button_widget'));
+
+
+    //         theme.insert(indx,{
+    //             xtype: "dropPanel",
+    //             cls: "dropPanel",
+    //             height: '100%',
+    //             margin: 5
+    //         });
+    //     }
+    // }],
     border: true,
-    items: [{
-        // xtype: 'component',
-        // autoEl: {
-        //     tag: 'button',
-        // },
-        xtype: 'component',
-        style: "text-align: center;",
-        height: '100%',
-        itemId: "button_panel",
-        padding: 10,
-        html: '<button type="button" class="edtBtn btn-default">Default</button>'
-    }],
+    initComponent: function () {
+         var me= this;
+         this.items = [
+            {
+                xtype: 'component',
+                style: "text-align: center;",
+                height: '100%',
+                itemId: "button_panel",
+                padding: 10,
+                html: '<button type="button" class="edtBtn btn-default">Default</button>'
+            }
+            // {
+            //     xtype: 'component',
+            //     text: 'Default',
+            //     // style: "text-align: center;",
+            //     // autoEl: {
+            //     //     tag: "button",
+            //     //     cls: "edtBtn btn-default",
+            //     //     html: "<span>Default</spam>"
+            //     // },
+            //     // itemSelector: "button.edtBtn",
+            //     // draggable: true,
+            //     draggable: {
+            //         delegate: "button",
+            //         constrain: true,
+            //         constrainTo: me.el
+            //     },
+            //     // ddGroup:"akshay",
+            //     // height: '100%',
+            //     // itemId: "button_panel",
+            //     // padding: 10,
+            //     html: '<button type="button" class="edtBtn btn-default">Default</button>'
+            // }
+
+         ]
+         this.callParent(arguments);
+    },
     listeners: {
         afterrender: function(panel) {
             var header = panel.getHeader();
+            window.abc= header;
             panel.getEl().on('mouseover', function() {
                 // header.getTools().forEach(function(tool) {
-                //     tool.show();
-                header.show()
+                    // tool.show();
+                header.show();
+                // header.el.applyStyles('display:block;');
                 // console.log(header);
                 // })
             }, this);
@@ -109,7 +205,13 @@ Ext.define("CaptivePortal.view.editor.ButtonWidget",{
                 //     tool.hide();
                 // })
                 header.hide();
+                // header.el.applyStyles('display:none;');
             }, this);
         }
+        // onRender: function(b){
+        //     var c = new Ext.dd.DropTarget(this.getView().el, {
+        //         ddGroup: d.ddGroup
+        //     });
+        // }
     }
 });
--- app/view/editor/EditorButtonSetting.js
+++ app/view/editor/EditorButtonSetting.js
@@ -5,82 +5,238 @@ Ext.define("CaptivePortal.view.editor.EditorButtonSetting",{
     closable : true,
     cls: 'editor_button_setting',
     layout: 'vbox',
-    items: [
-        {
-            xtype: 'panel',
-            width: '100%',
-            items: [
-                {
-                    xtype: 'textfield',
-                    cls: 'btn_text',
-                    listeners: {
-                        'change': function(){
-                            var button_panel = Ext.ComponentQuery.query('#'+this.up('.editor_button_setting').button_id)[0]
-                            button_panel.down('#button_panel').el.query('.edtBtn')[0].textContent = this.value
-                            var btn_json = Ext.decode(button_panel.button_json, true);
-                            btn_json['text'] = this.value;
-                            button_panel.button_json = JSON.stringify(btn_json);
-                        }
-                    }
-                }
-            ]
-        },
-        {
-            xtype: 'panel',
-            width: '100%',
-            items: [
-                {
-                    xtype: 'sliderfield',
-                    width: 100,
-                    value: 5,
-                    increment: 5,
-                    minValue: 0,
-                    maxValue: 25,
-                    listeners: {
-                        change: function(slider, newValue, thumb, eOpts){
-                            var button_panel = Ext.ComponentQuery.query('#'+this.up('.editor_button_setting').button_id)[0]
-                            var btn = button_panel.down('#button_panel').el.query('.edtBtn')[0]
-                            var newPadding = newValue+"px"+" "+newValue*2+"px"
-                            btn.style.padding = newPadding
+    initComponent: function () {
+        var btn_json = Ext.decode(this.btn_json);
+        this.items = [
+            {
+                xtype: 'panel',
+                layout: 'vbox',
+                width: '100%',
+                items: [
+                    {
+                        xtype: 'label',
+                        margin: '10 10 10 10',
+                        text: 'Align'
+                    },
+                    {
+                        xtype: 'panel',
+                        margin: '0 10 0 10',
+                        layout: 'hbox',
+                        items: [
+                            {
+                                xtype: 'button',
+                                margin: '0 20 0 0',
+                                text: 'Left',
+                                handler: function(){
+                                    var button_panel = Ext.ComponentQuery.query('#'+this.up('.editor_button_setting').button_id)[0]
+                                    var btn = button_panel.down('#button_panel')
+                                    btn.el.dom.style.textAlign = 'left'
+                                }
+                            },
+                            {
+                                xtype: 'button',
+                                margin: '0 20 0 0',
+                                text: 'Center',
+                                handler: function(){
+                                    var button_panel = Ext.ComponentQuery.query('#'+this.up('.editor_button_setting').button_id)[0]
+                                    var btn = button_panel.down('#button_panel')
+                                    btn.el.dom.style.textAlign = 'center'
+                                }
+                            },
+                            {
+                                xtype: 'button',
+                                margin: '0 20 0 0',
+                                text: 'Right',
+                                handler: function(){
+                                    var button_panel = Ext.ComponentQuery.query('#'+this.up('.editor_button_setting').button_id)[0]
+                                    var btn = button_panel.down('#button_panel')
+                                    btn.el.dom.style.textAlign = 'right'
+                                }
+                            }
+                        ]
 
-                            var btn_json = Ext.decode(button_panel.button_json, true);
-                            btn_json['padding_val'] = newValue;
-                            button_panel.button_json = JSON.stringify(btn_json);
+                    }
+                ]
+            },
+            {
+                xtype: 'panel',
+                layout: 'vbox',
+                width: '100%',
+                items: [
+                    {
+                        xtype: 'label',
+                        margin: '10 10 10 10',
+                        text: 'Text'
+                    },
+                    {
+                        xtype: 'textfield',
+                        margin: '0 10 0 10',
+                        width: '100%',
+                        cls: 'btn_text',
+                        value: btn_json["text"],
+                        listeners: {
+                            'change': function(){
+                                var button_panel = Ext.ComponentQuery.query('#'+this.up('.editor_button_setting').button_id)[0]
+                                button_panel.down('#button_panel').el.query('.edtBtn')[0].textContent = this.value
+                                var btn_json = Ext.decode(button_panel.button_json, true);
+                                btn_json['text'] = this.value;
+                                button_panel.button_json = JSON.stringify(btn_json);
+                            }
                         }
                     }
-                }
-            ]
-        },
-        {
-            xtype: 'tabpanel',
-            width: '100%',
-            items: [
-                {
-                    title: "Text",
-                    items: [
-                        {
-                            xtype: 'colorpicker'
+                ]
+            },
+            {
+                xtype: 'panel',
+                layout: 'vbox',
+                width: '100%',
+                items: [
+                    {
+                        xtype: 'label',
+                        margin: '10 10 10 10',
+                        text: 'Font Size'
+                    },
+                    {
+                        xtype: 'numberfield',
+                        margin: '0 10 0 10',
+                        width: '100%',
+                        minValue: 0,
+                        value: btn_json["font_size"],
+                        listeners: {
+                            'change': function(ths, newValue, oldValue, eOpts){
+                                var button_panel = Ext.ComponentQuery.query('#'+this.up('.editor_button_setting').button_id)[0]
+                                var btn = button_panel.down('#button_panel').el.query('.edtBtn')[0]
+                                btn.style.fontSize = newValue+"px"
+
+                                var btn_json = Ext.decode(button_panel.button_json, true);
+                                btn_json['font_size'] = newValue;
+                                button_panel.button_json = JSON.stringify(btn_json);
+                            }
                         }
-                    ]
-                },
-                {
-                    title: "Button",
-                    items: [
-                        {
-                            xtype: 'colorpicker'
+                    }
+                ]
+            },
+            {
+                xtype: 'panel',
+                layout: 'vbox',
+                width: '100%',
+                items: [
+                    {
+                        xtype: 'label',
+                        margin: '10 10 10 10',
+                        text: 'Radius'
+                    },
+                    {
+                        xtype: 'numberfield',
+                        margin: '0 10 0 10',
+                        width: '100%',
+                        minValue: 0,
+                        value: btn_json["border_radius"],
+                        listeners: {
+                            'change': function(ths, newValue, oldValue, eOpts){
+                                var button_panel = Ext.ComponentQuery.query('#'+this.up('.editor_button_setting').button_id)[0]
+                                var btn = button_panel.down('#button_panel').el.query('.edtBtn')[0]
+                                btn.style.borderRadius = newValue+"px"
+
+                                var btn_json = Ext.decode(button_panel.button_json, true);
+                                btn_json['border_radius'] = newValue;
+                                button_panel.button_json = JSON.stringify(btn_json);
+                            }
                         }
-                    ]
-                }
-            ]
-        }
-    ],
+                    }
+                ]
+            },
+            {
+                xtype: 'panel',
+                layout: 'vbox',
+                width: '100%',
+                items: [
+                    {
+                        xtype: 'label',
+                        margin: '10 10 10 10',
+                        text: 'Size'
+                    },
+                    {
+                        xtype: 'sliderfield',
+                        margin: '0 10 0 10',
+                        width: '100%',
+                        value: btn_json["padding_val"],
+                        increment: 5,
+                        minValue: 0,
+                        maxValue: 25,
+                        listeners: {
+                            change: function(slider, newValue, thumb, eOpts){
+                                var button_panel = Ext.ComponentQuery.query('#'+this.up('.editor_button_setting').button_id)[0]
+                                var btn = button_panel.down('#button_panel').el.query('.edtBtn')[0]
+                                var newPadding = newValue+"px"+" "+newValue*2+"px"
+                                btn.style.padding = newPadding
+
+                                var btn_json = Ext.decode(button_panel.button_json, true);
+                                btn_json['padding_val'] = newValue;
+                                button_panel.button_json = JSON.stringify(btn_json);
+                            }
+                        }
+                    }
+                ]
+            },
+            {
+                xtype: 'tabpanel',
+                width: '100%',
+                items: [
+                    {
+                        title: "Text",
+                        items: [
+                            {
+                                xtype: 'colorpicker',
+                                itemId: 'btn_txt_color',
+                                value: btn_json["txt_color"],
+                                listeners: {
+                                    select: function( ths, color, eOpts ){
+                                        var button_panel = Ext.ComponentQuery.query('#'+this.up('.editor_button_setting').button_id)[0]
+                                        var btn = button_panel.down('#button_panel').el.query('.edtBtn')[0]
+                                        btn.style.color = '#'+color;
+
+                                        var btn_json = Ext.decode(button_panel.button_json, true);
+                                        btn_json['txt_color'] = color;
+                                        button_panel.button_json = JSON.stringify(btn_json);
+                                    }
+                                }
+                            }
+                        ]
+                    },
+                    {
+                        title: "Button",
+                        items: [
+                            {
+                                xtype: 'colorpicker',
+                                itemId: 'btn_bg_color',
+                                value: btn_json["bg_color"],
+                                listeners: {
+                                    select: function( ths, color, eOpts ){
+                                        var button_panel = Ext.ComponentQuery.query('#'+this.up('.editor_button_setting').button_id)[0]
+                                        var btn = button_panel.down('#button_panel').el.query('.edtBtn')[0]
+                                        btn.style.background = '#'+color;
+
+                                        var btn_json = Ext.decode(button_panel.button_json, true);
+                                        btn_json['bg_color'] = color;
+                                        button_panel.button_json = JSON.stringify(btn_json);
+                                    }
+                                }
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+        this.callParent(arguments);
+    },
     listeners: {
         afterrender: function(){
-            var button_panel = Ext.ComponentQuery.query('#'+this.button_id)[0]
-            var btn_json = Ext.decode(button_panel.button_json, true)
-            console.log("........editor_button_setting render.......");
-            this.query("textfield")[0].setValue(btn_json["text"]);
-            this.query("sliderfield")[0].setValue(btn_json["padding_val"])
+            // var button_panel = Ext.ComponentQuery.query('#'+this.button_id)[0]
+            // var btn_json = Ext.decode(button_panel.button_json, true)
+            // console.log("........editor_button_setting render.......");
+            // this.query("textfield")[0].setValue(btn_json["text"]);
+            // this.query("sliderfield")[0].setValue(btn_json["padding_val"]);
         }
     },
 });

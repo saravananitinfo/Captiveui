37a776e8eb3fe56f40c21218b6dcb707b2dbcd21
ankur <ankur@ubuntu.ubuntu-domain>
2016-01-12 16:46:00 +0530

    textual changes

--- app/model/splash_template/SplashTemplate.js
+++ app/model/splash_template/SplashTemplate.js
@@ -5,6 +5,7 @@ Ext.define('CaptivePortal.model.splash_template.SplashTemplate',{
 		{ name:'name', type: 'string'},
 		{ name:'splash_content', type: 'auto'},
 		{ name:'status', type:'string'},
-		{ name:'category', type:'string'}
+		{ name:'category', type:'string'},
+		{ name:'associated_resource'}
 	]
 });
